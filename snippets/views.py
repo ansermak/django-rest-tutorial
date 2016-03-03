6c74cb8030f914ca0f8066837f44bdd6cefe9fc5
Anton Makrushyn <a.makrushin@synergetica.net>
2016-03-03 17:54:55 +0200

    Fix DjangoModelPermissions with FBV

--- snippets/views.py
+++ snippets/views.py
@@ -3,7 +3,7 @@ from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics, permissions, renderers
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -50,6 +50,7 @@ class SnippetHighLight(generics.GenericAPIView):
 
 
 @api_view(('GET',))
+@permission_classes((permissions.AllowAny,))
 def api_root(request, format=None):
     return Response({
         'users': reverse('user-list', request=request, format=format),
