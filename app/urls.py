8db595a34d71152976286bfbe53e2501f590eeec
Anton Makrushyn <a.makrushin@synergetica.net>
2016-03-03 12:53:31 +0200

    Replace function-base views with class-base views

--- app/urls.py
+++ app/urls.py
@@ -45,7 +45,7 @@ router.register(r'snippets', SnippetViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
     url(r'^', include('snippets.urls')),
 ]
