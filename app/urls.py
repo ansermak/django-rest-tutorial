437b8c32f65ad14b3700a0221196158f27afe2f8
Anton Makrushin <a.makrushin@synergetica.net>
2016-03-04 15:09:10 +0200

    Fix little error. End of tutorial

--- app/urls.py
+++ app/urls.py
@@ -24,7 +24,7 @@ router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    url(r'^', include('snippets.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
 ]
